48c72b12f4e9b32e88e5f547ac2ea59bc2649efa
Timur-dzh <timur.dzh@gmail.com>
2017-09-05 22:53:04 +0600

    1. save() - убран цикл 2. getall() - без двойного цикла

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,77 +3,64 @@
 // (powered by Fernflower decompiler)
 //
 
+import java.lang.reflect.Array;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int msize = 0;
+
     public ArrayStorage() {
     }
 
     void clear() {
-        for (int i=0; i < msize; i++)
-        {
-            storage[i]= null;
+        for (int i = 0; i < msize; i++) {
+            storage[i] = null;
         }
         msize = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < msize; ++i) {
-            if (this.storage[i] == null) {
-                this.storage[i] = r;
-                msize++;
-                break;
-            }
-        }
+        msize++;
+        this.storage[msize-1] = r;
     }
 
     Resume get(String uuid) {
-        Resume result =null;
+        Resume result = null;
 
-            for (int i = 0; i < msize; ++i) {
-                if (this.storage[i].uuid.equals(uuid)) {
-                    result = this.storage[i];
-                    break;
-                }
+        for (int i = 0; i < msize; ++i) {
+            if (this.storage[i].uuid.equals(uuid)) {
+                result = this.storage[i];
+                break;
             }
+        }
         return result;
     }
 
     void delete(String uuid) {
         for (int i = 0; i < msize; ++i) {
             if (this.storage[i].uuid.equals(uuid)) {
-                this.storage[i] = storage[msize-1];
-                this.storage[size()-1]=null;
+                this.storage[i] = storage[msize - 1];
+                this.storage[msize - 1] = null;
                 break;
             }
         }
-      msize--;
+        msize--;
     }
 
     Resume[] getAll() {
-        if (msize== 0)
+        if (msize <= 0)
             return new Resume[0];
 
         else {
-            int position = 0;
-
             Resume[] result = new Resume[msize];
-
-            for (int j = 0; j < result.length; ++j) {
-                for (int i = position; i < msize; ++i) {
-                    if (this.storage[i] != null) {
-                        result[j] = this.storage[i];
-                        position = i;
-                        break;
-                    }
-                }
+            for (int i = 0; i < msize; ++i) {
+                result[i] = this.storage[i];
             }
             return result;
         }
     }
 
     int size() {
-
         return msize;
     }
 }
